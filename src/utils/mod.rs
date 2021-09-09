6f13984e4d5539967f4e7675a3353f7b51d1acec
Matthias Behr <mbehr@mcbehr.de>
2021-09-09 20:37:39 +0200

    utils: add buffer_sort_messages
    This is a first naiive implementation.
    Cannot be used for real use-cases as this wont work if the recording
    started later than the buffer_time_us from lifecycle start!

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -3,10 +3,11 @@ use std::sync::mpsc::{Receiver, Sender};
 // const MS_PER_SEC:u32 = 1_000;
 
 /// const for micro-secs (us) per second
-pub const US_PER_SEC:u64 = 1_000_000;
+pub const US_PER_SEC: u64 = 1_000_000;
 
 pub fn utc_time_from_us(time_us: u64) -> chrono::NaiveDateTime {
-    chrono::NaiveDateTime::from_timestamp_opt( // todo get rid of all those mult/%... 
+    chrono::NaiveDateTime::from_timestamp_opt(
+        // todo get rid of all those mult/%...
         (time_us / US_PER_SEC) as i64,
         1_000u32 * (time_us % 1_000_000) as u32,
     )
@@ -78,11 +79,133 @@ pub fn buffer_sort_elements<T>(
     }
 }
 
+/// Struct/Wrapper around DltMessage that adds std::cmp::Ord based on the lifecycle and timestamp
+///
+/// If the lifecycle is the same only the timestamp is used. If the lifecycle is different the lifecycle start times are considered as well.
+struct SortedDltMessage {
+    m: crate::dlt::DltMessage,
+    calculated_time: u64, // lc.start_time + m.timestamp_us
+}
+impl std::cmp::PartialEq for SortedDltMessage {
+    fn eq(&self, other: &Self) -> bool {
+        self.calculated_time == other.calculated_time // todo index as well?
+    }
+}
+impl std::cmp::Ord for SortedDltMessage {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        if self.m.lifecycle == other.m.lifecycle {
+            if self.m.timestamp_dms == other.m.timestamp_dms {
+                self.m.index.cmp(&other.m.index) // keep the initial order on same timestamp
+            } else {
+                self.m.timestamp_dms.cmp(&other.m.timestamp_dms)
+            }
+        } else {
+            if self.calculated_time == other.calculated_time {
+                self.m.index.cmp(&other.m.index) // keep the initial order on same timestamp
+            } else {
+                self.calculated_time.cmp(&other.calculated_time)
+            }
+        }
+    }
+}
+impl std::cmp::PartialOrd for SortedDltMessage {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+impl std::cmp::Eq for SortedDltMessage {}
+
+/// sort DltMessages by time
+///
+/// The messages get buffered for the specified `buffered_time_us`.
+/// #### Note Make sure that the messages are not delayed/buffered longer than the timeframe. Otherwise the result will not be sorted correctly.
+/// #### Note The lifecycle start times are not changed during the processing but are cached with the first value. So if the times slightly change any messages from parallel lifecycles will be wrongly sorted.
+pub fn buffer_sort_messages<'a, M, S>(
+    inflow: Receiver<crate::dlt::DltMessage>,
+    outflow: Sender<crate::dlt::DltMessage>,
+    lcs_r: &'a evmap::ReadHandle<
+        crate::lifecycle::LifecycleId,
+        crate::lifecycle::LifecycleItem,
+        M,
+        S,
+    >,
+    buffer_time_us: u64,
+) -> Result<(), std::sync::mpsc::SendError<crate::dlt::DltMessage>>
+where
+    S: std::hash::BuildHasher + Clone,
+    M: 'static + Clone,
+{
+    let mut buffer = std::collections::VecDeque::<SortedDltMessage>::new();
+    // cache with lifecycle start times:
+    // lets not use a vec which would work for most cases but for the lifecycle ids can be larger for longer runs (e.g. processing multiple files)
+    let mut lc_map = std::collections::BTreeMap::<crate::lifecycle::LifecycleId, u64>::new();
+    // todo why mut for get_lc_start_time???
+    let mut get_lc_start_time = |ref x: crate::lifecycle::LifecycleId| -> u64 {
+        match lc_map.get(x) {
+            Some(t) => *t,
+            None => {
+                // get from lcr, add to map and return
+                let start_time = match lcs_r.read() {
+                    Some(map_read_ref) => {
+                        let l = map_read_ref.get_one(x);
+                        match l {
+                            Some(l) => l.start_time,
+                            None => 0,
+                        }
+                    },
+                    None => 0,
+                };
+                lc_map.insert(*x, start_time);
+                println!("added lc_map {} {}", x, start_time);
+                start_time
+            }
+        }
+    };
+
+    for m in inflow {
+        let last_reception_time_us = m.reception_time_us;
+        // add message sorted into buffer
+        let calculated_time: u64 = if m.is_ctrl_request() {
+            if m.index<100 {println!("m.is_ctrl_request!");}
+            m.reception_time_us
+        } else {
+            get_lc_start_time(m.lifecycle) + m.timestamp_us()
+        };
+        let sm = SortedDltMessage { m, calculated_time };
+        let idx = buffer.binary_search(&sm).unwrap_or_else(|x| x); // this is not stable but shouldn't matter as we added index to cmp::Ord
+        if sm.m.index < 100 {
+            println!("adding calc_time={} lrt={} buf={} idx={} len={}", calculated_time, last_reception_time_us, last_reception_time_us-calculated_time, idx, buffer.len());
+        } 
+
+        buffer.insert(idx, sm);
+        // remove all messages from buffer that have a time more than buffer_time_us earlier
+        loop {
+            match buffer.front() {
+                Some(sm) => {
+                    if sm.calculated_time + buffer_time_us < last_reception_time_us {
+                        let sm2 = buffer.pop_front().unwrap();
+                        outflow.send(sm2.m)?;
+                    } else {
+                        break; // msgs are sorted so we stop here and check after next msg
+                    }
+                }
+                None => {
+                    break;
+                }
+            }
+        }
+    }
+    for sm in buffer.into_iter() {
+        outflow.send(sm.m)?;
+    }
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::dlt::DltMessage;
     use crate::utils::*;
     use std::sync::mpsc::channel;
-    use crate::{dlt::DltMessage};
     //    use std::time::Instant;
     #[test]
     fn buffer_messages() {
