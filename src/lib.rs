c7fc05f820347e369d34e95c10b66fc39d167db5
Matthias Behr <mbehr@mcbehr.de>
2022-01-10 21:40:22 +0100

    fix(lib): version returning package version
    added test coverage as well.

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,28 @@ pub fn name() -> &'static str {
     "adlt"
 }
 pub fn version() -> (u32, u32, u32) {
-    (0, 0, 1)
+    const VERSION_MAJOR: &str = env!("CARGO_PKG_VERSION_MAJOR");
+    const VERSION_MINOR: &str = env!("CARGO_PKG_VERSION_MINOR");
+    const VERSION_PATCH: &str = env!("CARGO_PKG_VERSION_PATCH");
+    (
+        VERSION_MAJOR.parse::<u32>().unwrap_or(0),
+        VERSION_MINOR.parse::<u32>().unwrap_or(0),
+        VERSION_PATCH.parse::<u32>().unwrap_or(0),
+    )
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    #[test]
+    fn lib_name_version() {
+        const NAME: &str = env!("CARGO_PKG_NAME");
+        assert_eq!(NAME, name());
+        const VERSION: &str = env!("CARGO_PKG_VERSION");
+        let (major, minor, patch) = version();
+        assert_eq!(VERSION, format!("{}.{}.{}", major, minor, patch));
+    }
+
     #[test]
     fn mut_vec_elems() {
         let mut buffer = vec![1, 2, 3, 5, 8];
