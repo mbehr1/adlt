9c6862ac840db45dfbb463a0ca210f879e47a8b1
Matthias Behr <mbehr@mcbehr.de>
2022-01-17 23:31:42 +0100

    fix(convert): remove debug output for uint args
    and added a real testcase.

--- src/dlt/mod.rs
+++ src/dlt/mod.rs
@@ -738,12 +738,12 @@ impl DltMessage {
                     }
                 }
                 if is_uint {
-                    write!(
+                    /*write!(
                         text,
                         "<uint {} {:?}>",
                         arg.payload_raw.len(),
                         arg.payload_raw
-                    )?;
+                    )?;*/
                     match arg.payload_raw.len() {
                         1 => {
                             let val: u8 = arg.payload_raw[0];
@@ -1287,6 +1287,7 @@ pub fn parse_dlt_with_storage_header(
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::utils::*;
 
     mod dlt_char4 {
         use super::*;
@@ -1630,7 +1631,6 @@ mod tests {
 
     mod dlt_message {
         use super::*;
-        use crate::utils::*;
 
         #[test]
         fn basic_info() {
@@ -1768,4 +1768,48 @@ mod tests {
             assert_eq!(args_iter.next().unwrap().payload_raw, vec!(0));
         }
     }
+
+    #[test]
+    fn real_ex1() {
+        // wireshark dlt dissector problem? Shows Buffer too short with 6th arg (type info 0x10 = bool with undefined tyle 0 -> we default to 1 byte for that case )
+        let v = hex_to_bytes("3d 0a 00 af 4d 4d 4d 41 00 00 03 48 00 75 7d 16 41 08 4c 52 4d 46 55 44 53 00 00 82 00 00 1c 00 46 69 6e 61 6c 20 61 6e 73 77 65 72 20 61 72 72 69 76 65 64 20 61 66 74 65 72 20 00 23 00 00 00 93 01 00 00 00 82 00 00 21 00 75 73 20 66 72 6f 6d 20 74 68 65 20 6a 6f 62 20 68 61 6e 64 6c 65 72 20 5b 73 74 61 74 65 3a 20 00 00 82 00 00 0a 00 41 6e 73 77 65 72 69 6e 67 00 00 82 00 00 0b 00 2c 20 61 6e 73 77 65 72 3a 20 00 10 00 00 00 01 00 82 00 00 10 00 5d 20 66 6f 72 20 72 65 71 75 65 73 74 20 23 00 43 00 00 00 dc 05 00 00").unwrap();
+        assert_eq!(175, v.len());
+        let sh = DltStorageHeader {
+            secs: 0,
+            micros: 0,
+            ecu: DltChar4::from_str("ECU1").unwrap(),
+        };
+        let stdh = DltStandardHeader::from_buf(&v).unwrap();
+        let payload_offset = stdh.std_ext_header_size() as usize;
+        let m = DltMessage::from(
+            1423084,
+            sh,
+            stdh,
+            &v[DLT_MIN_STD_HEADER_SIZE..payload_offset],
+            v[payload_offset..].to_vec(),
+        );
+        assert_eq!(m.mcnt(), 10);
+        assert!(m.extended_header.is_some());
+        assert!(!m.is_big_endian());
+        assert!(m.is_verbose());
+        assert_eq!(m.ecu, DltChar4::from_str("MMMA").unwrap());
+        assert_eq!(m.apid().unwrap(), &DltChar4::from_str("LRMF").unwrap());
+        assert_eq!(m.ctid().unwrap(), &DltChar4::from_str("UDS").unwrap());
+        assert_eq!(m.noar(), 8);
+        let args: Vec<DltArg> = m.into_iter().collect();
+        for a in &args {
+            println!("{:?}", a);
+        }
+        assert_eq!(args.len(), 8);
+        assert!(args.iter().all(|a| !a.is_big_endian));
+
+        assert_eq!(m.payload_as_text().unwrap(), "Final answer arrived after  403 us from the job handler [state:  Answering , answer:  true ] for request # 1500");
+
+        let a = &args[0];
+        assert!(!a.is_big_endian);
+
+        println!("{:?}", m);
+    }
+
+    // todo add smaller test cases for all SINT, UINT, FLOAT, VARI, FIXP, STRING encodings,...
 }
