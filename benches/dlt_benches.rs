04b60f5368d458c74bbebce42f9ef0dc4c957af7
Matthias Behr <mbehr@mcbehr.de>
2022-02-19 07:20:10 +0100

    perf: header_as_text_to_write

--- benches/dlt_benches.rs
+++ benches/dlt_benches.rs
@@ -3,6 +3,7 @@ use criterion::{
 };
 use std::fs::File;
 use std::io::prelude::*;
+use std::io::BufWriter;
 use tempfile::NamedTempFile;
 
 use adlt::dlt::*;
@@ -40,6 +41,33 @@ pub fn dlt_bench_buf_as_hex_to_write(c: &mut Criterion) {
             let mut s = String::with_capacity(buf.len() * 3);
             assert!(buf_as_hex_to_write(&mut s, &buf).is_ok());
             assert_eq!(hex_to_bytes(&s).unwrap(), buf);
+        })
+    });
+}
+
+pub fn dlt_header_as_text_to_write(c: &mut Criterion) {
+    let v = hex_to_bytes("3d 0a 00 af 4d 4d 4d 41 00 00 03 48 00 75 7d 16 41 08 4c 52 4d 46 55 44 53 00 00 82 00 00 1c 00 46 69 6e 61 6c 20 61 6e 73 77 65 72 20 61 72 72 69 76 65 64 20 61 66 74 65 72 20 00 23 00 00 00 93 01 00 00 00 82 00 00 21 00 75 73 20 66 72 6f 6d 20 74 68 65 20 6a 6f 62 20 68 61 6e 64 6c 65 72 20 5b 73 74 61 74 65 3a 20 00 00 82 00 00 0a 00 41 6e 73 77 65 72 69 6e 67 00 00 82 00 00 0b 00 2c 20 61 6e 73 77 65 72 3a 20 00 10 00 00 00 01 00 82 00 00 10 00 5d 20 66 6f 72 20 72 65 71 75 65 73 74 20 23 00 43 00 00 00 dc 05 00 00").unwrap();
+    let sh = DltStorageHeader {
+        secs: (1640995200000000 / adlt::utils::US_PER_SEC) as u32,
+        micros: 471815,
+        ecu: DltChar4::from_buf(b"ECU1"),
+    };
+    let stdh = DltStandardHeader::from_buf(&v).unwrap();
+    let payload_offset = stdh.std_ext_header_size() as usize;
+    let m = DltMessage::from_headers(
+        1423084,
+        sh,
+        stdh,
+        &v[DLT_MIN_STD_HEADER_SIZE..payload_offset],
+        v[payload_offset..].to_vec(),
+    );
+
+    c.bench_function("header_as_text_to_write", |b| {
+        b.iter(|| {
+            let v = Vec::with_capacity(1024);
+            let mut writer = BufWriter::new(v);
+
+            assert!(m.header_as_text_to_write(&mut writer).is_ok());
         })
     });
 }
@@ -149,6 +177,7 @@ criterion_group!(
     dlt_benches,
     dlt_bench_is_storage_header_pattern,
     dlt_bench_buf_as_hex_to_write,
+    dlt_header_as_text_to_write,
     dlt_bench1,
     dlt_bench2
 );
