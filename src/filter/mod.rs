495349232d0bbedc4c29daf17157762314e229da
Matthias Behr <mbehr@mcbehr.de>
2023-11-11 10:51:50 +0100

    feat: support regex for ecu, apid, ctid
    Add support for regex (non-utf8) for ecu, apid and ctid.
    Json filter format extended with: ecuIsRegex, apidIsRegex, ctidIsRegex.
    Autodetection  based on regex special chars if ...isRegex is not provided.
    Dlt-Viewer filter format support for enableregexp_Appid, enableregexp_Context.
    Dlt-Viewer filter format does not support regex for ecu. No
    autodetection for ecu if imported as dlt-viewer filter (dlf) but if
    imported as json filter.

--- src/filter/mod.rs
+++ src/filter/mod.rs
@@ -1,4 +1,5 @@
 mod filter_impl;
+pub use self::filter_impl::Char4OrRegex;
 pub use self::filter_impl::Filter;
 pub use self::filter_impl::FilterKind;
 pub use self::filter_impl::FilterKindContainer;
