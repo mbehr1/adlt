26e6224c876cea2d3b3f4b752ad0a35f1b0aab06
Matthias Behr <mbehr@mcbehr.de>
2024-03-12 18:09:30 +0100

    perf: use par_iter for queries/non streams as well

--- src/bin/adlt/remote.rs
+++ src/bin/adlt/remote.rs
@@ -1609,85 +1609,15 @@ fn process_file_context<T: Read + Write>(
     // in any stream any messages to send?
     let all_msgs_len = fc.all_msgs.len();
     for stream in &mut fc.streams {
-        // more messages avail?
         let last_all_msgs_last_processed_len = stream.all_msgs_last_processed_len;
-        if all_msgs_len > last_all_msgs_last_processed_len {
-            if stream.filters_active {
-                let have_pos_filters = !stream.filters[FilterKind::Positive].is_empty();
-                let have_event_filters = !stream.filters[FilterKind::Event].is_empty();
+        process_stream_new_msgs(
+            stream,
+            last_all_msgs_last_processed_len,
+            &fc.all_msgs[last_all_msgs_last_processed_len..],
+            3_000_000,
+        );
 
-                // check msgs from _processed_len to all_msgs_len
-                // todo use parallel iterator for !is_stream as well!
-                // break after some max time/max amount of messages to improve reaction time
-
-                if stream.is_stream {
-                    // use parallel iterator
-                    let max_idx =
-                        std::cmp::min(all_msgs_len, last_all_msgs_last_processed_len + 3_000_000);
-                    let mut matching_idxs: Vec<usize> = fc.all_msgs
-                        [last_all_msgs_last_processed_len..max_idx]
-                        .par_iter()
-                        .enumerate()
-                        .filter(|(_i, msg)| {
-                            match_filters(
-                                msg,
-                                &stream.filters,
-                                have_pos_filters,
-                                have_event_filters,
-                            )
-                        })
-                        .map(|(i, _msg)| last_all_msgs_last_processed_len + i)
-                        .collect();
-                    // rayon collect seems to keep the order. not needed: matching_idxs.par_sort_unstable();
-                    // https://github.com/rayon-rs/rayon/issues/551#issuecomment-371657900
-                    /*
-                    // check whether matching_idxs is sorted:
-                    if !matching_idxs.windows(2).all(|w| w[0] < w[1]) {
-                        warn!(
-                            log,
-                            "par stream search from {}..max_idx={} matching_idxs.len={} not sorted!",
-                            last_all_msgs_last_processed_len,
-                            max_idx,
-                            matching_idxs.len()
-                        );
-                        matching_idxs.par_sort_unstable();
-                    }*/
-                    stream.filtered_msgs.append(&mut matching_idxs);
-                    stream.all_msgs_last_processed_len = max_idx;
-                } else {
-                    let mut i = last_all_msgs_last_processed_len;
-                    while i < all_msgs_len {
-                        let msg: &adlt::dlt::DltMessage = &fc.all_msgs[i];
-                        let matches = match_filters(
-                            msg,
-                            &stream.filters,
-                            have_pos_filters,
-                            have_event_filters,
-                        );
-
-                        if matches {
-                            stream.filtered_msgs.push(i);
-                            // for !is_stream end as soon as enough are filtered/found
-                            if !stream.is_stream
-                                && stream.filtered_msgs.len() >= stream.msgs_to_send.end
-                            {
-                                i += 1;
-                                break;
-                            }
-                        }
-                        i += 1;
-                        if i % 1_000_000 == 0 {
-                            // process 1mio msgs as a chunk
-                            break;
-                        }
-                    }
-                    stream.all_msgs_last_processed_len = i;
-                }
-            } else {
-                stream.all_msgs_last_processed_len = all_msgs_len;
-            }
-        }
-
+        // any new msgs for this stream to send to the remote side?
         let stream_msgs_len = if stream.filters_active {
             stream.filtered_msgs.len()
         } else {
@@ -1711,6 +1641,7 @@ fn process_file_context<T: Read + Write>(
             websocket.write_message(Message::Binary(encoded))?;
         }
 
+        // any new msgs for this stream to send to the remote side?
         if stream.msgs_sent.end < stream.msgs_to_send.end && stream.msgs_sent.end < stream_msgs_len
         {
             // send some more...
@@ -1818,6 +1749,73 @@ fn process_file_context<T: Read + Write>(
         fc.streams.retain(|stream| !stream.is_done);
     }
     Ok(())
+}
+
+// process any new msgs (or remaining) from all_msgs for this stream
+fn process_stream_new_msgs(
+    stream: &mut StreamContext,
+    new_msgs_offset: usize,
+    new_msgs: &[adlt::dlt::DltMessage],
+    max_chunk_size: usize,
+) {
+    if !new_msgs.is_empty() {
+        let new_msgs_len = new_msgs.len();
+        if stream.filters_active {
+            let have_pos_filters = !stream.filters[FilterKind::Positive].is_empty();
+            let have_event_filters = !stream.filters[FilterKind::Event].is_empty();
+
+            // break after some searching max_chunk_size of messages to improve reaction time
+            let max_idx = std::cmp::min(new_msgs_len, max_chunk_size);
+            let get_matching_idxs = || -> Vec<usize> {
+                new_msgs[0..max_idx]
+                    .par_iter()
+                    .enumerate()
+                    .filter(|(_i, msg)| {
+                        match_filters(msg, &stream.filters, have_pos_filters, have_event_filters)
+                    })
+                    .map(|(i, _msg)| new_msgs_offset + i) // done in parallel
+                    .collect() // serializing here
+            };
+
+            if stream.is_stream {
+                // we want to filter/identify all stream msgs even though the window
+                // might just send a few ones...
+                let mut matching_idxs: Vec<usize> = get_matching_idxs();
+                // rayon collect seems to keep the order. not needed: matching_idxs.par_sort_unstable();
+                // https://github.com/rayon-rs/rayon/issues/551#issuecomment-371657900
+                stream.filtered_msgs.append(&mut matching_idxs);
+                stream.all_msgs_last_processed_len = new_msgs_offset + max_idx;
+            } else {
+                // for non streams (aka queries) we need to consider that the window might change later via stream_change_window.
+                // the problem is: how to end up once enough items are collected
+                // we cannot do this with an atomic cnt and while_some as the adding/comparing
+                // would then be done in parallel.
+                // so we just use a full search and then limit the results:
+                // we could iterate over a few max_chunk_size fragments... (but might not be worth the benefit)
+
+                let max_matching = stream.msgs_to_send.end;
+                if stream.filtered_msgs.len() < max_matching {
+                    let nr_wanted = max_matching - stream.filtered_msgs.len();
+
+                    let mut matching_idxs: Vec<usize> = get_matching_idxs();
+
+                    // if we found more than wanted, we need to ensure that the next search
+                    // restarts at the unwanted one...
+                    if matching_idxs.len() <= nr_wanted {
+                        stream.all_msgs_last_processed_len = new_msgs_offset + max_idx;
+                    } else {
+                        // found more than wanted:
+                        let first_unwanted = matching_idxs[nr_wanted];
+                        stream.all_msgs_last_processed_len = first_unwanted;
+                        matching_idxs.truncate(nr_wanted);
+                    }
+                    stream.filtered_msgs.append(&mut matching_idxs);
+                }
+            }
+        } else {
+            stream.all_msgs_last_processed_len += new_msgs_len;
+        }
+    }
 }
 
 fn match_filters(
@@ -2059,7 +2057,10 @@ fn create_parser_thread(
 #[cfg(test)]
 mod tests {
     use super::*;
+    use adlt::dlt::{DltMessage, DltStandardHeader};
     use adlt::*;
+    const DLT_STD_HDR_HAS_TIMESTAMP: u8 = 1 << 4;
+
     // use serde_json::ser::to_string;
     use portpicker::pick_unused_port;
     use slog::{o, Drain, Logger};
@@ -2200,6 +2201,193 @@ mod tests {
         println!("sc with windows={:?}", sc); // we can debug print it
         assert_eq!(sc.msgs_to_send.start, 1);
         assert_eq!(sc.msgs_to_send.end, 2);
+    }
+
+    fn msg_for_test(index: u32) -> DltMessage {
+        let timestamp_us = 100u64 * (1 + index as u64);
+        DltMessage {
+            index,
+            reception_time_us: 100_000 + timestamp_us,
+            ecu: if index % 2 == 0 {
+                DltChar4::from_buf(b"ECU0")
+            } else {
+                DltChar4::from_buf(b"ECU1")
+            },
+            timestamp_dms: (timestamp_us / 100) as u32,
+            standard_header: DltStandardHeader {
+                htyp: DLT_STD_HDR_HAS_TIMESTAMP,
+                len: 0,
+                mcnt: 0,
+            },
+            extended_header: None,
+            payload: [].to_vec(),
+            payload_text: None,
+            lifecycle: 0,
+        }
+    }
+
+    #[test]
+    fn process_stream_new_msgs_no_filters() {
+        let log = new_logger();
+        let mut sc = StreamContext::from(&log, "stream", r#"{"filters":[]}"#).unwrap();
+        assert_eq!(sc.all_msgs_last_processed_len, 0);
+        process_stream_new_msgs(&mut sc, 0, &[], 1);
+        assert_eq!(sc.all_msgs_last_processed_len, 0);
+        process_stream_new_msgs(&mut sc, 0, &[], 1);
+        assert_eq!(sc.all_msgs_last_processed_len, 0);
+        process_stream_new_msgs(&mut sc, 0, &[msg_for_test(0)], 1);
+        assert_eq!(sc.filtered_msgs.len(), 0); // shall not be added if no filters are active
+        assert_eq!(sc.all_msgs_last_processed_len, 1);
+    }
+
+    #[test]
+    fn process_stream_new_msgs_is_stream_no_window() {
+        let log = new_logger();
+        let mut sc =
+            StreamContext::from(&log, "stream", r#"{"filters":[{"type":0,"ecu":"^ECU."}]}"#)
+                .unwrap();
+        assert!(sc.is_stream);
+        assert_eq!(sc.msgs_sent.start, 0); // no msgs sent yet
+        assert_eq!(sc.msgs_sent.end, 0);
+
+        assert_eq!(sc.msgs_to_send.start, 0); // wanted from idx 0
+        assert!(sc.msgs_to_send.end >= 2); // to larger...
+
+        assert_eq!(sc.all_msgs_last_processed_len, 0);
+
+        let msgs = [msg_for_test(0), msg_for_test(1)];
+
+        process_stream_new_msgs(&mut sc, 0, &msgs[0..0], 10);
+        assert_eq!(sc.all_msgs_last_processed_len, 0);
+
+        process_stream_new_msgs(&mut sc, 0, &msgs[0..1], 10);
+        assert_eq!(sc.filtered_msgs.len(), 1); // and idx = 0...
+        assert_eq!(sc.filtered_msgs, [0]);
+        assert_eq!(sc.all_msgs_last_processed_len, 1);
+
+        process_stream_new_msgs(&mut sc, 10, &msgs[1..2], 1);
+        assert_eq!(sc.filtered_msgs.len(), 2); // and idx = 10...
+        assert_eq!(sc.filtered_msgs, [0, 10]);
+        assert_eq!(sc.all_msgs_last_processed_len, 11);
+    }
+
+    #[test]
+    fn process_stream_new_msgs_query_window() {
+        let log = new_logger();
+        let mut sc = StreamContext::from(
+            &log,
+            "query",
+            r#"{"filters":[{"type":0,"ecu":"^ECU."}],"window":[1,2]}"#,
+        )
+        .unwrap();
+        assert!(!sc.is_stream);
+        assert_eq!(sc.msgs_sent.start, 1); // no msgs sent yet, wanted only idx 1... (so assuming we do fill all filtered_msgs)
+        assert_eq!(sc.msgs_sent.end, 1);
+
+        assert_eq!(sc.msgs_to_send.start, 1); // wanted from idx 0
+        assert_eq!(sc.msgs_to_send.end, 2); // to 2
+
+        assert_eq!(sc.all_msgs_last_processed_len, 0);
+
+        let msgs = [msg_for_test(0), msg_for_test(1), msg_for_test(2)];
+
+        process_stream_new_msgs(&mut sc, 0, &msgs[0..0], 10);
+        assert_eq!(sc.all_msgs_last_processed_len, 0);
+
+        process_stream_new_msgs(&mut sc, 0, &msgs[0..1], 10);
+        assert_eq!(sc.filtered_msgs.len(), 1); // and idx = 0...
+        assert_eq!(sc.filtered_msgs, [0]);
+        assert_eq!(sc.all_msgs_last_processed_len, 1);
+
+        process_stream_new_msgs(&mut sc, 10, &msgs[1..2], 1);
+        assert_eq!(sc.filtered_msgs.len(), 2); // and idx = 10...
+        assert_eq!(sc.filtered_msgs, [0, 10]);
+        assert_eq!(sc.all_msgs_last_processed_len, 11);
+
+        process_stream_new_msgs(&mut sc, 20, &msgs[2..], 10);
+        assert_eq!(sc.filtered_msgs.len(), 2); // no more wanted.
+        assert_eq!(sc.filtered_msgs, [0, 10]);
+        assert_eq!(sc.all_msgs_last_processed_len, 11); // we keep the old one to continue searh on window change.
+
+        // change the window: (now we want more msgs)
+        sc.msgs_to_send.end = 1000;
+        process_stream_new_msgs(&mut sc, 20, &msgs[2..], 10);
+        assert_eq!(sc.filtered_msgs.len(), msgs.len());
+        assert_eq!(sc.filtered_msgs, [0, 10, 20]);
+        assert_eq!(sc.all_msgs_last_processed_len, 21);
+    }
+
+    #[test]
+    fn process_stream_new_msgs_query_window_large() {
+        let log = new_logger();
+        let mut sc = StreamContext::from(
+            &log,
+            "query",
+            r#"{"filters":[{"type":0,"ecu":"ECU0"}],"window":[0,100]}"#,
+        )
+        .unwrap();
+        assert!(!sc.is_stream);
+        assert_eq!(sc.msgs_sent.start, 0); // no msgs sent yet, wanted only idx 1... (so assuming we do fill all filtered_msgs)
+        assert_eq!(sc.msgs_sent.end, 0);
+
+        assert_eq!(sc.msgs_to_send.start, 0); // wanted from idx 0
+        assert_eq!(sc.msgs_to_send.end, 100); // to 2
+
+        assert_eq!(sc.all_msgs_last_processed_len, 0);
+
+        let mut msgs = vec![];
+        for i in 1..=1000 {
+            msgs.push(msg_for_test(i as u32));
+        }
+
+        process_stream_new_msgs(&mut sc, 0, &msgs[0..0], 5000);
+        assert_eq!(sc.all_msgs_last_processed_len, 0);
+
+        process_stream_new_msgs(&mut sc, 0, &msgs[0..], 500);
+        assert_eq!(sc.filtered_msgs.len(), 100); // we do only want 100 from current window
+
+        assert_eq!(sc.filtered_msgs[0..5], [1, 3, 5, 7, 9]);
+        for (idx, val) in sc.filtered_msgs.iter().enumerate() {
+            assert_eq!(*val, (idx * 2) + 1, "#{} = {}", idx, val);
+        }
+        assert_eq!(
+            sc.all_msgs_last_processed_len,
+            201,
+            "got {} filtered_msgs",
+            sc.filtered_msgs.len()
+        );
+
+        // next call should not change anything:
+        process_stream_new_msgs(&mut sc, 201, &msgs[201..], 500);
+        assert_eq!(sc.filtered_msgs.len(), 100); // we do only want 100 from current window
+        assert_eq!(sc.all_msgs_last_processed_len, 201);
+
+        // change the window: (now we want more msgs)
+        sc.msgs_to_send.end = 1000;
+        // trigger to stop with less than wanted (so due to max_chunk_size)
+        process_stream_new_msgs(&mut sc, 201, &msgs[201..], 500);
+        assert_eq!(sc.filtered_msgs.len(), 350);
+        assert_eq!(sc.all_msgs_last_processed_len, 701);
+        for (idx, val) in sc.filtered_msgs.iter().enumerate() {
+            assert_eq!(*val, (idx * 2) + 1, "#{} = {}", idx, val);
+        }
+
+        // now ending due to end of all msgs
+        process_stream_new_msgs(&mut sc, 701, &msgs[701..801], 500);
+        assert_eq!(sc.all_msgs_last_processed_len, 801);
+        assert_eq!(sc.filtered_msgs.len(), 400);
+        for (idx, val) in sc.filtered_msgs.iter().enumerate() {
+            assert_eq!(*val, (idx * 2) + 1, "#{} = {}", idx, val);
+        }
+
+        // now ending due to end of window = end of all msgs
+        process_stream_new_msgs(&mut sc, 801, &msgs[801..], 500);
+        assert_eq!(sc.all_msgs_last_processed_len, 1000);
+        assert_eq!(sc.filtered_msgs.len(), 500);
+        for (idx, val) in sc.filtered_msgs.iter().enumerate() {
+            assert_eq!(*val, (idx * 2) + 1, "#{} = {}", idx, val);
+        }
+        assert_eq!(sc.all_msgs_last_processed_len, 1000);
     }
 
     use std::{collections::HashMap, time::Duration};
