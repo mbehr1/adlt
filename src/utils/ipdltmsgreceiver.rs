77b4af6c14d8d9e43ef63044491bb5be41b977bc
Matthias Behr <mbehr@mcbehr.de>
2025-07-12 08:54:08 +0200

    test(receive): basic create_send_socket tests

--- src/utils/ipdltmsgreceiver.rs
+++ src/utils/ipdltmsgreceiver.rs
@@ -708,6 +708,44 @@ mod tests {
     }
 
     #[test]
+    fn test_create_send_socket_udp() {
+        let port = pick_unused_port().expect("no ports free");
+        let interface = InterfaceIndexOrAddress::Address("127.0.0.1".parse().unwrap());
+        let socket = create_send_socket(
+            RecvMode::Udp,
+            SocketAddr::new("127.0.0.1".parse().unwrap(), port),
+            interface,
+        );
+        assert!(socket.is_ok());
+        drop(socket);
+
+        let interface = InterfaceIndexOrAddress::Address("127.0.0.1".parse().unwrap());
+        let socket = create_send_socket(
+            RecvMode::UdpMulticast,
+            SocketAddr::new("127.0.0.1".parse().unwrap(), port),
+            interface,
+        );
+        assert!(socket.is_err());
+
+        let interface = InterfaceIndexOrAddress::Address("127.0.0.1".parse().unwrap());
+        let socket = create_send_socket(
+            RecvMode::UdpMulticast,
+            SocketAddr::new("224.0.0.1".parse().unwrap(), port),
+            interface,
+        );
+        assert!(socket.is_ok());
+        drop(socket);
+
+        let interface = InterfaceIndexOrAddress::Address("127.0.0.1".parse().unwrap());
+        let socket = create_send_socket(
+            RecvMode::Udp,
+            SocketAddr::new("224.0.0.1".parse().unwrap(), port),
+            interface,
+        );
+        assert!(socket.is_err());
+    }
+
+    #[test]
     fn test_ip_dlt_msg_receiver_creation_udp_m() {
         let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(224, 0, 0, 1)), 12345);
         let interface = InterfaceIndexOrAddress::Address("127.0.0.1".parse().unwrap());
